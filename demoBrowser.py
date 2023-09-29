53757d810d1f20d9727a3e0f4335d337a8ff5e10
Bkr05041992 <kartheek1624@gmail.com>
2023-09-29 03:28:38 +0000

    add driver, service

--- demoBrowser.py
+++ demoBrowser.py
@@ -1,2 +1,8 @@
 from selenium import webdriver
-print('kartheek')
+from selenium.webdriver.chrome.service import Service
+service_obj = Service() # service is responsible for start & close of driver
+print('service : ', service_obj)
+driver = webdriver.Chrome(service=service_obj)
+print('driver : ',driver)
+driver.get('https://www.facebook.com/')
+
